8bb7099694d15ffd24ad901293c68b17f286785d
yanggam2-kmh <yanggam2@kmh.hs.kr>
2026-04-02 09:59:04 +0900

    Update queue_dequeue.py
    정규 표현식 반영

--- queue_dequeue.py
+++ queue_dequeue.py
@@ -1,30 +1,30 @@
 SIZE = 5
-Queue = ['A','B','C',None, None]
+queue = ['A','B','C',None, None]
 front = -1
 rear = 2
 
-def isEmpty():
+def is_empty():
     if(rear == front):
         return 1
     else:
         return 0
 
-def deQueue():
+def de_queue():
     global front
-    if(isEmpty()):
+    if(is_empty()):
         print("큐가 공백 상태입니다")
         return
     front += 1
-    delete_data = Queue[front]
-    Queue[front] = None
+    delete_data = queue[front]
+    queue[front] = None
     print("삭제할 데이터: ", delete_data)
 
-def printQueue():
+def print_queue():
     print("--현재 큐의 상태--")
     print("[", end= " ")
     for i in range(SIZE):
-        print(Queue[i], end = " ")
+        print(queue[i], end = " ")
     print("]")
     print("front: ", front, "rear: ", rear)
 
-print(Queue); deQueue(); print(Queue); deQueue(); print(Queue); deQueue(); print(Queue); deQueue();
+print(queue); de_queue(); print(queue); de_queue(); print(queue); de_queue(); print(queue); de_queue();
